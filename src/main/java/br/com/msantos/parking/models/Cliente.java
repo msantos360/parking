52b09c331db2afb1a376da445025287d3d114f3c
Michael Santos <michael.shel96@gmail.com>
2019-07-08 19:28:02 -0300

    configuração do banco de dados e adição da entidade cliente

---
+++ src/main/java/br/com/msantos/parking/models/Cliente.java
@@ -0,0 +1,93 @@
+package br.com.msantos.parking.models;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column(nullable = false, unique = true)
+	private Long cpf;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Calendar dataNascimento;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar dataCadastro = Calendar.getInstance();
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private TipoCliente tipo = TipoCliente.NORMAL;
+
+	/** Hibernate only **/
+	@Deprecated
+	public Cliente() {
+	}
+
+	public TipoCliente getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setDataCadastro(Calendar dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Calendar getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
